3e72dcd4952e5039afb4d5bd468cb86242bf0ff2
Andre Almeida <almeidaandreluz@gmail.com>
2022-03-11 05:57:04 -0300

    Desafio concluido

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,13 +1,21 @@
-import { Response, Request } from "express";
+import { Response, Request } from 'express'
 
-import { CreateUserUseCase } from "./CreateUserUseCase";
+import { CreateUserUseCase } from './CreateUserUseCase'
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+	constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+	handle(request: Request, response: Response): Response {
+		try {
+			const { email, name } = request.body
+
+			const newUser = this.createUserUseCase.execute({ email, name })
+
+			return response.status(201).json(newUser)
+		} catch (error) {
+			return response.status(400).json({ error: error.message })
+		}
+	}
 }
 
-export { CreateUserController };
+export { CreateUserController }
