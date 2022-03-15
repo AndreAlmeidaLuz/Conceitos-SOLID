d4084168012dfcb30b83d7e26357d33e8c503c5c
Andre Almeida <almeidaandreluz@gmail.com>
2022-03-15 05:28:34 -0300

    Desafio completo

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
-import { app } from ".";
+import { app } from '.'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
 
-app.listen(3333, () => console.log("Server is running!"));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.listen(3333, () => console.log('Server is running!'))
